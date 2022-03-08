caa8d41d928e99b6476042235fd6425119cd863e
Marco Fracassi <fracassi.marco@gmail.com>
2022-03-08 09:24:05 +0100

    refactoring

--- src/main/kotlin/BowlingGameFactory.kt
+++ src/main/kotlin/BowlingGameFactory.kt
@@ -1,11 +1,9 @@
 class BowlingGameFactory {
-    fun create(): BowlingGame {
-        return BowlingGame(Accumulator(FrameCounter(), listOf(
+    fun create() = BowlingGame(Accumulator(FrameCounter(), listOf(
             MissBuilder(),
             SpareBuilder(),
             StrikeBuilder(),
             NumberPointBuilder()
         )))
-    }
 }
 
